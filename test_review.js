f29affbc3c1649935de20edcaa72a31495b3eab2
Yoscroll <ianhenry.c808@gmail.com>
2016-11-23 08:41:17 -1000

    added concat

--- test_review.js
+++ test_review.js
@@ -16,10 +16,10 @@ var hero={
 
 function swordInTheStone(worthy){
   if (worthy == "Arthur"){
-    console.log("We have found our King worthy");
+    console.log("We have found our King "+worthy);
   }
   else{
-    console.log("We must keep searching, for worthy is unworthy");
+    console.log("We must keep searching, for "+worthy+" is unworthy");
   }
 }
 swordInTheStone(knight);
@@ -28,7 +28,7 @@ swordInTheStone(knight);
 
 function trueLove(king,queen){
   if (queen=="Guinevere"&&king=="Arthur"){
-    console.log("On this day, our king and queen shall rule happily ever after");
+    console.log("On this day, our "+king+" and "+queen+" shall rule happily ever after");
   }
   else{
     console.log("We must keep searching, for out true love must reign over Camelot");
@@ -40,13 +40,13 @@ trueLove(knight,maiden);
 
 function epicBattle(wizard,enemy,power){
   if (power<=15){
-    console.log("Our wizard has fallen");
+    console.log("Our "+wizard+" has fallen");
   }
   else if (power>=16 && power<=40){
-    console.log("Wizard has almost slain enemy");
+    console.log( wizard+"has almost slain enemy");
   }
   else{
-    console.log("Our wizard has slain the enemy");
+    console.log("Our "+wizard+" has slain the enemy");
   }
 }
 epicBattle(hero.wizard,monster,hero.power);
